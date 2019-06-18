c72efba981a01e959a398982a91a6ff8a6155244
ignaciofigueroa <ijfigueroa@gmail.com>
2019-06-18 11:06:47 -0400

    Grid

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'shopping_cart', to: 'shopping_cart#show'
+  get 'shopping_cart/add/:product_id/:amount', to: 'shopping_cart#add_product', defaults: {amount: 1}
+
+  get 'shopping_cart/update'
   resources :orders
   resources :products
   devise_for :users
   resources :users
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
